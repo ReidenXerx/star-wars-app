a62e688275f9b72b67c3d7d4113c683e48cd556a
ReidenXerx <vadim.lysenko@onmail.com>
2024-08-15 15:13:50 +0300

    feat(implementation): fixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,28 +4,38 @@ import { Hero, PaginatedResponse, Film, Starship, Planet } from '../types'
 export const fetchHeroes = async (
   page: number
 ): Promise<PaginatedResponse<Hero>> => {
-  const response = await axios.get(`people/?page=${page}`)
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL ?? ''}/people/?page=${page}`
+  )
   return response.data
 }
 
 export const fetchHeroDetails = async (heroId: number): Promise<Hero> => {
-  const response = await axios.get(`/people/${heroId}`)
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL ?? ''}/people/${heroId}`
+  )
   return response.data
 }
 
 export const fetchFilmDetails = async (filmId: number): Promise<Film> => {
-  const response = await axios.get(`/films/${filmId}`)
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL ?? ''}/films/${filmId}`
+  )
   return response.data
 }
 
 export const fetchStarshipDetails = async (
   starshipId: number
 ): Promise<Starship> => {
-  const response = await axios.get(`/starships/${starshipId}`)
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL ?? ''}/starships/${starshipId}`
+  )
   return response.data
 }
 
 export const fetchPlanetDetails = async (planetId: number): Promise<Planet> => {
-  const response = await axios.get(`/planets/${planetId}`)
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL ?? ''}/planets/${planetId}`
+  )
   return response.data
 }
